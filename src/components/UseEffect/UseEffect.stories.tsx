1b8d9790822f1718854129e4000953df701ec2dc
dnisaev <dnisaev@yandex.ru>
2024-01-13 22:39:11 +0300

    add setInterval setTimeout

--- src/components/UseEffect/UseEffect.stories.tsx
+++ src/components/UseEffect/UseEffect.stories.tsx
@@ -4,14 +4,14 @@ export default {
     title: 'UseEffect',
 }
 
-export const Example1 = () => {
-    console.log('Example1 render!!!');
+export const UseEffectExample = () => {
+    console.log('UseEffectExample render!!!');
     const [counter, setCounter] = useState(1);
     const [fake, setFake] = useState(0);
 
     useEffect(() => {
         console.log('useEffect every render');
-    }, );
+    },);
 
     useEffect(() => {
         console.log('useEffect like componentDidMount');
@@ -24,6 +24,34 @@ export const Example1 = () => {
     return <>
         <div>Counter, {counter}</div>
         <div>Fake, {fake}</div>
+        <button onClick={() => setCounter(counter + 1)}>counter +</button>
+        <button onClick={() => setFake(fake + 1)}>fake +</button>
+    </>
+}
+
+export const SetTimeoutSetIntervalExample = () => {
+    console.log('SetTimeoutExample render');
+    const [counter, setCounter] = useState(1);
+    const [fake, setFake] = useState(0);
+    const [timer, setTimer] = useState(0);
+
+    // useEffect(() => {
+    //     setTimeout(()=>{
+    //         console.log('setTimeout render')
+    //         document.title = counter.toString()
+    //     }, 1000);
+    // }, [counter]);
+
+    // useEffect(() => {
+    //     setInterval(() => {
+    //         setTimer(state => state + 1)
+    //     }, 1000);
+    // }, []);
+
+    return <>
+        <div>Counter, {counter}</div>
+        <div>Fake, {fake}</div>
+        <div>Timer, {timer}</div>
         <button onClick={() => setCounter(counter + 1)}>counter +</button>
         <button onClick={() => setFake(fake + 1)}>fake +</button>
     </>

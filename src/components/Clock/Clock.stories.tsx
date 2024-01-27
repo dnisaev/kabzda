5f25414bab59fe4ec3ebfdce1092fe7529e626e7
dnisaev <dnisaev@yandex.ru>
2024-01-27 17:21:38 +0300

    add analog Clock

--- src/components/Clock/Clock.stories.tsx
+++ src/components/Clock/Clock.stories.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import {Clock} from "./Clock";
 
 export default  {
-    component: <Clock/>,
+    component: <Clock mode={"digital"}/>,
 }
 
-export const Example1 = () => {
-    return <Clock/>
+export const DigitalExample = () => {
+    return <Clock mode={"digital"}/>
+}
+
+export const AnalogExample = () => {
+    return <Clock mode={"analog"}/>
 }
--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -1,8 +1,13 @@
 import React, {useEffect, useState} from "react";
+import styles from "./Clock.module.css";
+
+type ClockPropsType = {
+    mode: "digital" | "analog"
+}
 
 const get2digitsString = (num: number): string => num < 10 ? `0${num}` : `${num}`;
 
-export const Clock = () => {
+export const Clock = ({mode}: ClockPropsType) => {
 
     const [date, setDate] = useState(new Date());
 
@@ -19,11 +24,45 @@ export const Clock = () => {
 
     return (
         <div>
-            <span>{get2digitsString(date.getHours())}</span>
-            :
-            <span>{get2digitsString(date.getMinutes())}</span>
-            :
-            <span>{get2digitsString(date.getSeconds())}</span>
+            {
+                mode === "digital"
+                    ? <DigitalClockView date={date}/>
+                    : <AnalogClockView date={date}/>
+            }
         </div>
     )
+}
+
+type ClockViewPropsType = {
+    date: Date
+}
+
+export const DigitalClockView: React.FC<ClockViewPropsType> = ({date}) => {
+    return <>
+        <span>{get2digitsString(date.getHours())}</span>
+        :
+        <span>{get2digitsString(date.getMinutes())}</span>
+        :
+        <span>{get2digitsString(date.getSeconds())}</span>
+    </>
+}
+
+export const AnalogClockView: React.FC<ClockViewPropsType> = ({date}) => {
+
+    const secondsStyle = {
+        transform: `rotate(${date.getSeconds() * 6}deg)`
+    };
+    const minutesStyle = {
+        transform: `rotate(${date.getMinutes() * 6}deg)`
+    };
+    const hoursStyle = {
+        transform: `rotate(${date.getHours() * 30}deg)`
+    };
+    return <div className={styles.clock}>
+        <div className={styles.analogClock}>
+            <div className={`${styles.dial} ${styles.seconds}`} style={secondsStyle}/>
+            <div className={`${styles.dial} ${styles.minutes}`} style={minutesStyle}/>
+            <div className={`${styles.dial} ${styles.hours}`} style={hoursStyle}/>
+        </div>
+    </div>
 }

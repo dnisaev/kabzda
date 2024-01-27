c38d40ba0cc29bf314df758703135a935de7907b
dnisaev <dnisaev@yandex.ru>
2024-01-27 18:26:55 +0300

    add cleanup to UseEffect

--- src/components/UseEffect/UseEffect.stories.tsx
+++ src/components/UseEffect/UseEffect.stories.tsx
@@ -36,7 +36,7 @@ export const SetTimeoutSetIntervalExample = () => {
     const [timer, setTimer] = useState(0);
 
     useEffect(() => {
-        const timeout = setTimeout(()=>{
+        const timeout = setTimeout(() => {
             console.log('setTimeout render')
             document.title = counter.toString()
         }, 1000);
@@ -64,5 +64,50 @@ export const SetTimeoutSetIntervalExample = () => {
         <div>Timer, {timer}</div>
         <button onClick={() => setCounter(counter + 1)}>counter +</button>
         <button onClick={() => setFake(fake + 1)}>fake +</button>
+    </>
+}
+
+export const ResetEffectExample = () => {
+    const [counter, setCounter] = useState(1);
+
+    console.log('ResetEffectExample render');
+
+    useEffect(() => {
+        console.log("Effect occurred")
+
+        return () => {
+            console.log("Reset effect")
+        }
+    }, []);
+
+    const plusCounter = () => {
+        setCounter(counter + 1)
+    }
+
+    return <>
+        <div>Counter, {counter}</div>
+        <button onClick={plusCounter}>+</button>
+    </>
+}
+
+export const KeysTrackerExample = () => {
+    const [text, setText] = useState('');
+
+    console.log('ResetEffectExample render' + text);
+
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            console.log(e.key)
+            setText((text)=>text + e.key)
+        }
+        window.addEventListener('keypress', handler);
+
+        return () => {
+            window.removeEventListener('keypress', handler);
+        }
+    }, [text]);
+
+    return <>
+        <div>Text, {text}</div>
     </>
 }
